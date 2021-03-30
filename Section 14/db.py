f8f8282ce48289358fbf9bc88b5568549950f3eb
Suresh Yhap <sureshyhap@gmail.com>
2021-03-30 16:05:44 -0400

    Finished Section 14

---
+++ Section 14/db.py
@@ -0,0 +1,20 @@
+import mysql.connector
+
+con = mysql.connector.connect(
+    user="ardit700_student",
+    password="ardit700_student",
+    host="108.167.140.122",
+    database="ardit700_pm1database"
+)
+
+cursor = con.cursor()
+
+word = input("Enter a word: ")
+
+query = cursor.execute("SELECT * FROM Dictionary WHERE Expression = '{}'".format(word))
+
+results = cursor.fetchall()
+
+for result in results:
+    print(result[1])
+    print()
