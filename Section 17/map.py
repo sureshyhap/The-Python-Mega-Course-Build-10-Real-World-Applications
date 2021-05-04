613eb22e6b7571e55f4fe1a78f45b616fbe4ca02
Suresh Yhap <sureshyhap@gmail.com>
2021-05-04 12:00:41 -0400

    Started Section 19 App 3

--- Section 17/map.py
+++ Section 17/map.py
@@ -17,9 +17,24 @@ for i in range(data.shape[0]):
         color.append("red")
 
 map = folium.Map(location=[45, -36], zoom_start=6, tiles="Stamen Terrain")
-fg = folium.FeatureGroup(name="My Map")
+fg = folium.FeatureGroup(name="Volcanoes")
 for lt, ln, n, c in zip(lat, lon, name, color):
     fg.add_child(folium.Marker(location=[lt, ln], popup=n, icon=folium.Icon(color=c)))
-map.add_child(fg);
+f_obj = open ("world.json", "r", encoding="utf-8-sig")
+contents = f_obj.read()
+
+def pop(x):
+    if x["properties"]["POP2005"] < 10e6:
+        return {"fillColor" : "green"}
+    elif 10e6 <= x["properties"]["POP2005"] < 20e6:
+        return {"fillColor" : "orange"}
+    else:
+        return {"fillColor" : "red"}
+
+fg2 = folium.FeatureGroup(name="Population")    
+fg2.add_child(folium.GeoJson(data=contents, style_function=lambda x: pop(x)))
+map.add_child(fg)
+map.add_child(fg2)
+map.add_child(folium.LayerControl())
 map.save("Map1.html")
 
